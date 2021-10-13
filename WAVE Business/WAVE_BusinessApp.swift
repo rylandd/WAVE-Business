83058ff6821fffe389b1fdc9278c9d17e493a135
rylandd <rylandd@me.com>
2021-10-13 11:12:23 -0400

    Initial Commit

---
+++ WAVE Business/WAVE_BusinessApp.swift
@@ -0,0 +1,17 @@
+//
+//  WAVE_BusinessApp.swift
+//  WAVE Business
+//
+//  Created by Ryland Dreibelbis on 10/13/21.
+//
+
+import SwiftUI
+
+@main
+struct WAVE_BusinessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
