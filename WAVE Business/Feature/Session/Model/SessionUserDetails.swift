b8b38c2f5235630a1c0aba088571f99ae3653487
tsun99 <tsungaila99@gmail.com>
2021-10-17 17:13:50 -0400

    Added SessionService and SessionUserDetails so that the app can know if user is logged in

---
+++ WAVE Business/Feature/Session/Model/SessionUserDetails.swift
@@ -0,0 +1,14 @@
+//
+//  SessionUserDetails.swift
+//  WAVE Business
+//
+//  Created by Tomas Sungaila on 10/17/21.
+//
+
+import Foundation
+
+struct SessionUserDetails {
+    let firstName: String
+    let lastName: String
+    let barName: String
+}
---
+++ WAVE Business/Feature/Session/Service/SessionService.swift
@@ -0,0 +1,77 @@
+//
+//  SessionService.swift
+//  WAVE Business
+//
+//  Created by Tomas Sungaila on 10/17/21.
+//
+
+import Foundation
+import Combine
+import FirebaseAuth
+import FirebaseDatabase
+
+enum SessionState {
+    case loggedIn
+    case loggedOut
+}
+
+protocol SessionService {
+    var state: SessionState {get}
+    var userDetails: SessionUserDetails? {get}
+    func logout()
+}
+
+final class SessionServiceImpl: ObservableObject, SessionService {
+    
+    @Published var state: SessionState = .loggedOut
+    @Published var userDetails: SessionUserDetails?
+    
+    private var handler: AuthStateDidChangeListenerHandle?
+    
+    init(){
+        setupFirebaseAuthHandler()
+    }
+    
+    func logout() {
+        
+    }
+}
+private extension SessionServiceImpl {
+    
+    func setupFirebaseAuthHandler() {
+        
+        handler = Auth
+            .auth()
+            .addStateDidChangeListener{[weak self] res, user in
+                guard let self = self else {return}
+                self.state = user == nil ? .loggedOut : .loggedIn
+                if let uid = user?.uid {
+                    self.handleRefresh(with: uid)
+                }
+            }
+    }
+    
+    func handleRefresh(with uid: String){
+        
+        Database
+            .database()
+            .reference()
+            .child("users")
+            .child(uid)
+            .observe(.value) { [weak self] snapshot in
+                
+                guard let self = self,
+                      let value = snapshot.value as? NSDictionary,
+                      let firstName = value[RegistrationKeys.firstName.rawValue] as? String,
+                      let lastName = value[RegistrationKeys.lastName.rawValue] as? String,
+                      let barName = value[RegistrationKeys.barName.rawValue] as? String else {
+                          return
+                      }
+                DispatchQueue.main.async {
+                    self.userDetails = SessionUserDetails(firstName: firstName,
+                                                          lastName: lastName,
+                                                          barName: barName)
+                }
+            }
+    }
+}
