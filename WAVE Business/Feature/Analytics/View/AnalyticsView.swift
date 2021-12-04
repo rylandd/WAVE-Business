4fb266c1e2420b3a5506950c663d7c12a3f4740b
William Hinson <wthinson@live.unc.edu>
2021-12-04 00:25:58 -0500

    Removed Charts from package dependencies and started on Analytics View

---
+++ WAVE Business/Feature/Analytics/View/AnalyticsView.swift
@@ -0,0 +1,32 @@
+//
+//  AnalyticsView.swift
+//  WAVE Business
+//
+//  Created by William Hinson on 12/3/21.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+struct AnalyticsView : View {
+    
+    
+    var body: some View {
+        
+        ZStack {
+            Image("Rectangle")
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.all)
+            
+            
+            VStack {
+                Image("logo")
+                    .frame(width: 228, height: 183, alignment: Alignment.center)
+                
+                
+            }
+        }
+    }
+}
